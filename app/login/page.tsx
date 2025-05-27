f9b9f365bcbc804679c633d1bc61e312982e2cf1
TanyaaSrivastava <tanyasrivastava2808@gmail.com>
2025-05-27 11:54:22 +0530

    added token key in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,30 +1,40 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
 export default function LoginPage() {
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
     });
-    const router = useRouter();
+   const [buttonDisabled, setButtonDisabled] = React.useState(false);
+   const [loading, setLoading] = React.useState(false);
 
     const onLogin = async () => {
         try {
             const response = await axios.post('/api/users/login', user);
-            console.log('Login success:', response.data);
-            router.push('/dashboard');
+            console.log('Login success:');
+            router.push('/profile');
         } catch (error: any) {
             console.error('Login error:', error.response?.data || error.message);
             alert("Login failed: " + (error.response?.data?.message || error.message));
+        
         }
-    };
+    }
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0){
+            setButtonDisabled(false);
+        } else{
+            setButtonDisabled(true);
+        }
+    }, [user]);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-2xl mb-4">Login</h1>
+            <h1 className="text-2xl mb-4">{loading ? "Processing" :"login"}</h1>
             <label htmlFor="email">E-mail</label>
             <input
                 className="p-2 border border-gray-300 rounded mb-4 w-full max-w-sm"
